813a73ef7afeb727a2c30380c89374f8de123957
Judd Ebert <judd.ebert@student.lovett.org>
2024-12-22 21:54:08 -0500

    Editing gitignore

--- myproject/myproject/views.py
+++ myproject/myproject/views.py
@@ -5,5 +5,12 @@ def homepage(request):
 
 def about(request):
     return render(request, 'about.html')
+
 def login(request):
     return render(request, 'login.html')
+
+def logout(request):
+    return logout(request, 'home.html')
+
+def homePage(request):
+    return render(request, 'homePage.html')
